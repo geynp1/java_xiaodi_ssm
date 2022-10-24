6c59e4cfa3d019bcbbebd52ddc59c7f1bfec8ca2
cliz <geynp123@hotmail.com>
2022-10-24 18:05:41 +0800

    第6章 配置文件及注解测试

--- src/main/java/net/xdclass/demoproject/controller/TestController.java
+++ src/main/java/net/xdclass/demoproject/controller/TestController.java
@@ -1,6 +1,8 @@
 package net.xdclass.demoproject.controller;
 
+import net.xdclass.demoproject.config.WXConfig;
 import net.xdclass.demoproject.utils.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,24 @@ public class TestController {
     @Value("${wxpay.secret}")
     private String paySecret;
 
-    @RequestMapping("get_config")
+    @Autowired
+    private WXConfig wxConfig;
+
+    @RequestMapping("get_config")//通过配置文件
     public JsonData testConfig(){
         Map<String, String> map = new HashMap<>();
         map.put("appid", payAppid);
         map.put("secret", paySecret);
+
+        return JsonData.buildSuccess(map);
+    }
+
+    @RequestMapping("get_config1")//通过配置类
+    public JsonData testConfig1(){
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", wxConfig.getPayAppid());
+        map.put("secret", wxConfig.getPaySecret());
+        map.put("mechid", wxConfig.getPayMechId());
 
         return JsonData.buildSuccess(map);
     }
