a3e141be0e069bbff662b59dacfc737f7f74c1de
cliz <geynp123@hotmail.com>
2022-10-28 17:34:45 +0800

    第9章

--- src/main/java/net/xdclass/demoproject/filter/LoginFilter.java
+++ src/main/java/net/xdclass/demoproject/filter/LoginFilter.java
@@ -1,9 +1,11 @@
 package net.xdclass.demoproject.filter;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.xdclass.demoproject.domain.User;
 import net.xdclass.demoproject.service.UserService;
 import net.xdclass.demoproject.service.impl.UserServiceImpl;
+import net.xdclass.demoproject.utils.JsonData;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
@@ -11,9 +13,15 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.PrintWriter;
 
 @WebFilter(urlPatterns = "/api/v1/pri/*",filterName = "loginFilter")
 public class LoginFilter implements Filter {
+
+    //jackson中把数据序列化的工具
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     /**
      * 容器加载时
      * @param filterConfig
@@ -34,17 +42,39 @@ public class LoginFilter implements Filter {
         if(StringUtils.isEmpty(token)){
             token = req.getParameter("token");
         }
-        if(StringUtils.isEmpty(token)){
-            return;
-        }else {
+        if(!StringUtils.isEmpty(token)){
             //判断token是否合法
             User user = UserServiceImpl.sessionMap.get(token);
             if (user != null) {
+                /**https://blog.csdn.net/qq_52530620/article/details/124393530
+                 * // (2)FilterChain.doFilter(ServletRequest,ServletResponse)
+                 * 交由下web容器进行数据处理如果有过滤器链，则交由下一个过滤器进行处理
+                 */
                 filterChain.doFilter(servletRequest,servletResponse);
                 System.out.println(123);
+            }else{//token失效了
+                JsonData jsonData = JsonData.buildError("登陆失败，token无效", -2);
+                String jsonStr = objectMapper.writeValueAsString(jsonData);
+                renderJson(resp,jsonStr);
             }
+        }else{
+            JsonData jsonData = JsonData.buildError("未登录", -3);
+            String jsonStr = objectMapper.writeValueAsString(jsonData);
+            renderJson(resp,jsonStr);
         }
 
+
+    }
+
+    //json数据写给前端
+    private void renderJson(HttpServletResponse response, String json) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
